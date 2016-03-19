1b462ff1a0bcc0efe1927371ea8a88bb7bf0f95c
Lindsey Baab <lindsey_baab@brown.edu>
2016-03-19 15:52:53 -0400

    fixed insertion sort

--- src/Sorting/InsertionSort.java
+++ src/Sorting/InsertionSort.java
@@ -8,8 +8,20 @@ public class InsertionSort {
 	 * @return					the input array, sorted least to greatest
 	 */
 	public static int[] insertionSort(int[] unsortedArray) {
-		//TODO: S2 implements insertion sort
-		
-		return null;
+		int temp;
+    for (int i = 1; i < array.length-1;i++){
+    	int j =i;
+    	while(j<0 && array[i-1] > array[i]){
+    		temp = array[i];
+    		array[i] = array[i-1];
+    		array[i-1] = temp;
+    		j= i -1;
+    		
+    		
+    	}
+    	
+    }
+
+    return array;
 	}
 }
