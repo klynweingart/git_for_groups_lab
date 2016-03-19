264c3bffcb07f7f1bce242abe3518b9285b55bba
Katherine Weingart <katherine_weingart@brown.edu>
2016-03-19 15:51:32 -0400

    fixed buble sort

--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -7,8 +7,22 @@ public class BubbleSort {
 	 * @return					the input array, sorted least to greatest
 	 */
 	public static int[] bubbleSort(int[] unsortedArray) {
-		//TODO: S1 implements bubble sort
-		
-		return null;
+		boolean unsorted = true;
+		int j = 0;
+		while(unsorted) {
+			unsorted = false;
+			for( int i = 0; i < unsortedArray.length() - 1; i++) {
+				if(unsortedArray[i] > unsortedArray[i+1]) {
+					unsorted = true;
+					swap(unsortedArray, i, i+1);
+				}
+			}
+		}
+	}
+
+	private static void swap(int[] arr, int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
 	}
 }
