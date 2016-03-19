cc271cb3a2a0ee924f605765a5586412f945c938
Katherine Weingart <katherine_weingart@brown.edu>
2016-03-19 15:57:07 -0400

    made changes to main so it runs bubble sort

--- src/Sorting/Main.java
+++ src/Sorting/Main.java
@@ -21,9 +21,12 @@ public class Main {
 		System.out.println();
 		
 		System.out.println("Sorted Array:");
-		//TODO: sort the array and print out its contents
-		
-		
+		BubbleSort.sort(unsortedArray);
+				
+				
+		for (int i: unsortedArray) {
+				System.out.print(i + " ");
+		}
 	}
 
 }
